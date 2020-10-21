bcc595781d48f6094317525aacadccbaf167e599
McFunston <mica.funston@gmail.com>
2020-10-21 14:57:44 -0400

    Initial

---
+++ EmailLinkFinder/EmailLinkFinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using Mono.Web;
+
+namespace EmailLinkFinder
+{
+    public class Link
+    {
+        public string FileName { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class EmailLinkFinder
+    {
+        public List<Link> GetLinks(string message)
+        {
+            List<Link> links = new List<Link>();
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(message);
+            HtmlNodeCollection rows = htmlDocument.DocumentNode.SelectNodes("//a[@href]");
+
+            foreach (HtmlAgilityPack.HtmlNode row in rows)
+            {
+                foreach (var at in row.Attributes)
+                {
+                    if (at.Name == "href")
+                    {
+                        Link link = new Link();
+                        link.Address = HttpUtility.HtmlDecode(at.Value);
+                        link.FileName = at.OwnerNode.FirstChild.InnerHtml;
+                        links.Add(link);
+                    }
+                }
+            }
+
+            return links;
+        }
+
+    }
+}
---
+++ LinkFinderTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using EmailLinkFinder;
+
+namespace LinkFinderTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string message = System.IO.File.ReadAllText("Subject.html");
+            EmailLinkFinder.EmailLinkFinder linkFinder = new EmailLinkFinder.EmailLinkFinder();
+
+            List<Link> list = linkFinder.GetLinks(message);
+
+            Console.WriteLine("Stop");
+
+        }
+    }
+}
